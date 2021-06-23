bf185cdec471d66cd3d6bd50adf090a6cd5560e0
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-06-23 23:05:05 +0100

    Exercícios 5

---
+++ Fundamentos/introducao-a-javascript-e-logica-de-programacao_1/script5.js
@@ -0,0 +1,18 @@
+let degreeAngleA = 100;
+let degreeAngleB = 40;
+let degreeAngleC = 40;
+
+
+let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+let allAnglesArePositives = degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
+
+
+if (allAnglesArePositives) {
+    if (sumOfAngles === 180) {
+        console.log(true);
+    } else {
+        console.log(false);
+    };
+} else {
+    console.log('Erro: ângulo inválido');
+}
