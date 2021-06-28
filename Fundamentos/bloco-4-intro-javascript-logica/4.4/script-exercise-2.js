54762094a183b947668a7d18375307f820f98e9a
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-06-28 22:47:36 +0100

    exercício 2

--- Fundamentos/bloco-4-intro-javascript-logica/4.4/script-exercise-2.js
+++ Fundamentos/bloco-4-intro-javascript-logica/4.4/script-exercise-2.js
@@ -1,10 +1,8 @@
-//Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-
+    recorrente: 'Sim',
 };
 
-console.log("Bem - vinda, " + info.personagem);
+console.log(info.recorrente);
