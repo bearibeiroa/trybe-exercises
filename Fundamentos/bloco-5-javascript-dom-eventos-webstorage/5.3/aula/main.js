8b52689462be94d4ea5f6c3b685beccfcf11c91a
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-06 20:24:57 +0100

    Gabarito de aula 5.3

--- Fundamentos/bloco-5-javascript-dom-eventos-webstorage/5.3/aula/main.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-webstorage/5.3/aula/main.js
@@ -8,18 +8,42 @@ const myWebpage = document.getElementById('my-spotrybefy');
  Copie esse arquivo e edite apenas ele;
 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-
-
-
-3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
-
-4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página;
-4.1. Que tal redirecionar para seu portifólio?
-
-5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
-
-Segue abaixo um exemplo do uso de event.target:
 */
+function handleChangeTech(event) {
+    let techElement = document.querySelector('.tech');
+    techElement.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value = '';
+}
+firstDiv.addEventListener('click', handleChangeTech);
+secondDiv.addEventListener('click', handleChangeTech);
+thirdDiv.addEventListener('click', handleChangeTech);
+
+//3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
+
+input.addEventListener('input', function(event) {
+    let techElement = document.querySelector('.tech');
+    techElement.innerText = event.target.value;
+})
+
+//4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página;
+//4.1.Que tal redirecionar para seu portifólio ?
+
+myWebpage.addEventListener('dblclick', function() {
+    window.location.replace('https://bearibeiroa.github.io/portfolio');
+})
+
+//5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
+myWebpage.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'red';
+})
+
+myWebpage.addEventListener('mouseout', function(event) {
+    event.target.style.color = 'unset';
+})
+
+
+//Segue abaixo um exemplo do uso de event.target:
 
 function resetText(event) {
     // O Event é passado como um parâmetro para a função.
