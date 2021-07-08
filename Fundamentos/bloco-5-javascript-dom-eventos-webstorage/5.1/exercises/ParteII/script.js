37da2cc63c4773e49b69b6e0dc8b0161865943b6
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-08 14:15:10 +0100

    refatoração com querySelector da ParteII

--- Fundamentos/bloco-5-javascript-dom-eventos-webstorage/5.1/exercises/ParteII/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-webstorage/5.1/exercises/ParteII/script.js
@@ -1,11 +1,12 @@
 let mainTitle = document.getElementById("header-container");
 mainTitle.style.backgroundColor = "#2DB069";
 
-let leftTasks = document.querySelectorAll(".emergency-tasks .emergengy-tasks-title");
+let leftTasks = document.querySelectorAll(".emergency-tasks .emergency-tasks-title");
 for (let index = 0; index < leftTasks.length; index += 1) {
     leftTasks[index].style.backgroundColor = "#A500F3";
 }
-let leftEmergencyTasks = document.querySelectorAll(".emergency-tasks")[0];
+
+let leftEmergencyTasks = document.querySelector(".emergency-tasks");
 leftEmergencyTasks.style.backgroundColor = "#FB9F84";
 
 let rightTasks = document.querySelectorAll(".no-emergency-tasks .no-emergency-tasks-title");
@@ -13,5 +14,5 @@ for (let index = 0; index < rightTasks.length; index += 1) {
     rightTasks[index].style.backgroundColor = "black";
 }
 
-let rightEmergencyTasks = document.querySelectorAll(".no-emergency-tasks")[0];
+let rightEmergencyTasks = document.querySelector(".no-emergency-tasks");
 rightEmergencyTasks.style.backgroundColor = "#F9DB5D";
