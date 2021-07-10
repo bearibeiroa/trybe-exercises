9e35866450ea0c7b19bd7d3818a8af1727422814
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-10 20:24:50 +0100

    Refatoração do exercício inteiro

--- Fundamentos/bloco-5-javascript-dom-eventos-webstorage/5.3/aula/main.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-webstorage/5.3/aula/main.js
@@ -1,58 +1,59 @@
-const firstDiv = document.getElementById('first-div');
-const secondDiv = document.getElementById('second-div');
-const thirdDiv = document.getElementById('third-div');
+const firstLi = document.getElementById('firstLi');
+const secondLi = document.getElementById('secondLi');
+const thirdLi = document.getElementById('thirdLi');
 const input = document.getElementById('input');
-const myWebpage = document.getElementById('my-spotrybefy');
+const myWebpage = document.getElementById('mySpotrybefy');
 
 /*
- Copie esse arquivo e edite apenas ele;
+1. Copie esse arquivo e edite apenas ele;
+
 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-*/
-function handleChangeTech(event) {
-    let techElement = document.querySelector('.tech');
-    techElement.classList.remove('tech');
-    event.target.classList.add('tech');
-    input.value = '';
+2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?*/
+
+function addClassClick(event) {
+    const techElement = document.querySelector('.tech');
+    techElement.classList.remove("tech");
+    event.target.classList.add("tech");
 }
-firstDiv.addEventListener('click', handleChangeTech);
-secondDiv.addEventListener('click', handleChangeTech);
-thirdDiv.addEventListener('click', handleChangeTech);
+firstLi.addEventListener('click', addClassClick);
+secondLi.addEventListener('click', addClassClick);
+thirdLi.addEventListener('click', addClassClick);
 
-//3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
+/* 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+com a classe 'tech'; */
 
-input.addEventListener('input', function(event) {
-    let techElement = document.querySelector('.tech');
-    techElement.innerText = event.target.value;
-})
-
-//4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página;
-//4.1.Que tal redirecionar para seu portifólio ?
-
-myWebpage.addEventListener('dblclick', function() {
-    window.location.replace('https://bearibeiroa.github.io/portfolio');
-})
-
-//5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
-myWebpage.addEventListener('mouseover', function(event) {
-    event.target.style.color = 'red';
-})
-
-myWebpage.addEventListener('mouseout', function(event) {
-    event.target.style.color = 'unset';
-})
+function changeText() {
+    const techElement = document.querySelector('.tech');
+    techElement.innerText = input.value;
+}
+input.addEventListener('keyup', changeText);
 
 
-//Segue abaixo um exemplo do uso de event.target:
+/* 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+redirecione para alguma página;
+4.1. Que tal redirecionar para seu portifólio? */
+function redirectiClick() {
+    window.location.href = "https://bearibeiroa.github.io/portfolio";
+}
+myWebpage.addEventListener('dblclick', redirectiClick);
+
+/* 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+a cor do mesmo; */
+
+function mouseMovimentIn() {
+    myWebpage.style.color = 'red'
+}
+myWebpage.addEventListener('mouseover', mouseMovimentIn);
+
+function mouseMovimentOut() {
+    myWebpage.style.color = 'white'
+}
+myWebpage.addEventListener('mouseleave', mouseMovimentOut);
+
 
 function resetText(event) {
-    // O Event é passado como um parâmetro para a função.
     event.target.innerText = 'Opção reiniciada';
-    // O event possui várias propriedades, porém a mais usada é o event.target,
-    // que retorna o objeto que disparou o evento.
 }
-
-firstDiv.addEventListener('dblclick', resetText);
-// Não precisa passar o parâmetro dentro do addEventListener. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'divUm'.
+firstLi.addEventListener('dblclick', resetText);
+secondLi.addEventListener('dblclick', resetText);
+thirdLi.addEventListener('dblclick', resetText);
