7dd7e29d4568e6f9f2e492cfeff9e0a52b4ef209
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-11 13:16:51 +0100

    Refatoração do exercício inteiro - questão 1

--- Fundamentos/bloco-5-javascript-dom-eventos-webstorage/5.3/exercices/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-webstorage/5.3/exercices/script.js
@@ -13,44 +13,28 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Exercício 1 - Inserir os dias do mês com as classes 'day' 'friday' 'holiday'
+// Escreva seu código abaixo.
 
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+/*1. Insira os dias de dezembro*/
+function decemberDays() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const monthDays = document.querySelector('#days');
 
-function DaysOfTheMonth() {
-    let getDaysList = document.querySelector('#days');
     for (let index = 0; index < dezDaysList.length; index += 1) {
-        let day = dezDaysList[index];
-        let dayNumber = document.createElement('li');
-
-        if (day === 24 | day === 31) {
-            dayNumber.className = 'day holiday';
-            dayNumber.innerHTML = day;
-            getDaysList.appendChild(dayNumber);
-        } else if (day === 4 | day === 11 | day === 18) {
-            dayNumber.className = 'day friday';
-            dayNumber.innerHTML = day;
-            getDaysList.appendChild(dayNumber);
-        } else if (day === 25) {
-            dayNumber.className = 'day holiday friday';
-            dayNumber.innerHTML = day;
-            getDaysList.appendChild(dayNumber);
-        } else {
-            dayNumber.className = 'day';
-            dayNumber.innerHTML = day;
-            getDaysList.appendChild(dayNumber);
+        const daysOfTheMonth = dezDaysList[index];
+        const daysMonthList = document.createElement('li');
+        daysMonthList.innerHTML = daysOfTheMonth;
+        daysMonthList.classList.add('day');
+        if (daysOfTheMonth === 24 | daysOfTheMonth === 31) {
+            daysMonthList.classList.add('holiday');
         }
+        if (daysOfTheMonth === 4 | daysOfTheMonth === 11 | daysOfTheMonth === 18) {
+            daysMonthList.classList.add('friday');
+        }
+        if (daysOfTheMonth === 25) {
+            daysMonthList.classList.add('friday', 'holiday');
+        }
+        monthDays.appendChild(daysMonthList);
     }
 }
-DaysOfTheMonth();
-
-//Exercício 2 - Insira o botão "Feriados" com ID "btn-holiday" dentro da <div class = "buttons-container">
-
-function holidayOfTheMonth() {
-    let holiday = document.querySelector(".buttons - container");
-    let holidayBtn = document.createElement('button');
-    holidayBtn.innerText = "Feriados";
-    holidayBtn.id = "btn-holiday";
-    holiday.appendChild(holidayBtn);
-}
-holidayOfTheMonth(Feriados)
+decemberDays()
