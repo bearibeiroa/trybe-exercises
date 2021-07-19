a13576fd27fbde2eb15eba33c954eefb3b705f3f
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-19 19:06:17 +0100

    atualização

--- Fundamentos/bloco-6-html-css-forms-flexbox-responsivo/6.1/exercise/script.js
+++ Fundamentos/bloco-6-html-css-forms-flexbox-responsivo/6.1/exercise/script.js
@@ -1,27 +1,21 @@
 function states() {
     const estados = [{ "label": "Acre", "value": "AC" }, { "label": "Alagoas", "value": "AL" }, { "label": "Amapá", "value": "AP" }, { "label": "Amazonas", "value": "AM" }, { "label": "Bahia", "value": "BA" }, { "label": "Ceará", "value": "CE" }, { "label": "Distrito Federal", "value": "DF" }, { "label": "Espírito Santo", "value": "ES" }, { "label": "Goiás'", "value": "GO" }, { "label": "Maranhão", "value": "MA" }, { "label": "Mato Grosso", "value": "MT" }, { "label": "Mato Grosso do Sul", "value": "MS" }, { "label": "Minas Gerais", "value": "MG" }, { "label": "Paraná", "value": "PR" }, { "label": "Paraíba", "value": "PB" }, { "label": "Pará", "value": "PA" }, { "label": "Pernambuco", "value": "PE" }, { "label": "Piau", "value": "PI" }, { "label": "Rio Grande do Norte", "value": "RN" }, { "label": "Rio Grande do Sul", "value": "RS" }, { "label": "Rio de Janeiro", "value": "RJ" }, { "label": "Rondônia", "value": "RO" }, { "label": "Roraima", "value": "RR" }, { "label": "Santa Catarina", "value": "SC" }, { "label": "Sergipe", "value": "SE" }, { "label": "São Paulo", "value": "SP" }, { "label": "Tocantins", "value": "TO" }];
-    const select = document.querySelector('.option-city');
+    const select = document.querySelector('.option-state');
     for (let index of estados) {
         let option = document.createElement('option');
-        option.innerText = index.label;
+        option.innerHTML = index.label;
         option.value = index.value;
         select.appendChild(option);
     }
 }
 states()
 
+function dateValidation() {
+    const date = document.querySelector('#date').value;
+    let arrayData = data.split(' ');
+    for (let index = 0; index < arrayData.length; index += 1){
+        if (index[2] == '/' && index[5] == '/' && !index[2] && !index[5]) {
 
-const submit = document.querySelector('#submit');
-
-function checkDate() {
-    const dateField = document.querySelector('.date-set').value;
-    const dateArray = dateField.split('/')
-    const date = parseInt(dateArray[0]);
-    const month = parseInt(dateArray[1]);
-    const year = parseInt(dateArray[2]);
-    if (!date > 0 && date <= 31) {
-        return { message: 'Data não foi preenchida!' }
-
+        }
     }
 }
-submit.addEventListener('click', checkDate)
