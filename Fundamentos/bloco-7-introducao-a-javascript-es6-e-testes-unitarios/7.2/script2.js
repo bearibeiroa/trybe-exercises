75dfca2dc0dd6b6ec4a3fdaeeb93b34ede3bbb5b
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-24 12:08:47 +0100

    Finaliza exercício 1 - parte II

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.2/script2.js
@@ -0,0 +1,27 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Questão 1
+const newElement = (obj, key, value) => {
+	obj[key] = value;
+	return obj;
+}
+console.log(newElement(lesson2,'turno','manhã'));
+
