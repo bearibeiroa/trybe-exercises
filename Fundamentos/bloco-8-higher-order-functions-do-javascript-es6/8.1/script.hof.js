899c9aa31613c79e7d00eb2438a514a365f584fd
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-29 18:41:16 +0100

    Atualiza script.hof e adiciona demais exercícios

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.1/script.hof.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.1/script.hof.js
@@ -1,6 +1,6 @@
-const newEmployees = (nomeCompleto, email) => {
+const newEmployees = () => {
   const employees = {
-    // Chamar uma função que passa o nome como parâmetro
+    // Criar uma função que recebe o nome como parâmetro
     id1: employeeGenerator('Pedro Guerra'),  // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
     id2: employeeGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
     id3: employeeGenerator('Carla Paiva')  // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
@@ -12,7 +12,5 @@ const employeeGenerator = (nomeCompleto) => {
   const email = nomeCompleto.toLowerCase().split(' ').join('_');
   return { nomeCompleto, email: `${email}@trybe.com` };
 }
-// Retornar a função principal usando como parâmetro a que cria o nome.
+// Retornar a função principal usando como parâmetro a que cria o nome./
 console.table(newEmployees(employeeGenerator));
-
-
