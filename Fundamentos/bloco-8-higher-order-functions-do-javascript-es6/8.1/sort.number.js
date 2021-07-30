988142db6fda1dd6c8984fd833f6bb2b02b204de
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-30 12:17:35 +0100

    Adiciona ajuste para consertar erro

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.1/sort.number.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.1/sort.number.js
@@ -1,15 +1,17 @@
 // 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
+// Cria número aleatório entre 1 e 5
 const randomNum = () => {
   return Math.floor(Math.random() * 5 + 1)
 }
-// console.log(sortNumbers());
 
+//Compara o número escolhido com o sorteado e retorna msg
 const sortResult = (number, randomNum) => {
   if (number === randomNum) {
-    return `Número sorteado: ${number}. Parabéns você ganhou`;
+    return `Número sorteado: ${randomNum}. Parabéns você ganhou`;
+  } else {
+    return `Número sorteado: ${randomNum}.Tente novamente`;
   }
-  return `Número sorteado: ${number}.Tente novamente`;
 }
 console.log(sortResult(3, randomNum()));
 
