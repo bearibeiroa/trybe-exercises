611174c2792049a4695134c03ee093a083d3ec72
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-31 19:44:20 +0100

    Exercício 1 parte 2

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.1/script.parte2.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.1/script.parte2.js
@@ -1,6 +1,16 @@
+// fatorial de um número significa n! = n * (n -1) * (n - 2) * ... 1
+
 const factorNum = number => {
-  for (let index = 1; index <= number; index += 1){
-    return number *= index;
+  let result = 1;
+    for (let index = 1; index <= number; index += 1){
+      result *= index;
+    }
+  return result;
   }
-}
-console.log(factorNum(5));
+console.log(factorNum(6));
+
+// Recursiva
+// Função recursiva é quando uma função chama a si mesma dentro do próprio bloco de código. 
+const recursiveFunc = num => num > 1 ? num * recursiveFunc(num - 1) : 1;
+console.log(recursiveFunc(6))
+
