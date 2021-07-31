f67f8335eb2787b5a0ca6f630b24afbd8c56a53d
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-31 15:49:41 +0100

    Atualização

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.3/exercise1.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.3/exercise1.js
@@ -66,7 +66,7 @@ const books = [
 // Adicione o código do exercício aqui:
 
 
-// Exercício 1
+// 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
 
 const expectedResult = [
   'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
@@ -85,3 +85,40 @@ console.log(formatedBookNames);
 
 
 assert.deepStrictEqual(formatedBookNames, expectedResult);
+
+
+// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+function nameAndAge() {
+  // escreva seu código aqui
+}
+
+
+assert.deepStrictEqual(nameAndAge(), expectedResult);
