53d33e08a2d39096fa5f51af2cc746e4f7b542bb
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-08-03 22:37:10 +0100

    atualização

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.3/Parte III/greetPeople.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+const greetPeople = (people) => {
+  let greeting = 'Hello ';
+
+  for (const person in people) {
+    greeting += people[person];
+  }
+  return greeting;
+};
+
+const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+
+assert.deepStrictEqual(typeof greetPeople, 'function');
+assert.strictEqual(greetPeople(parameter), result);
