9e3c4d07917a11b364af5bec2754fd30c9f6e316
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-08-18 00:05:51 +0100

    questão 1

--- Fundamentos/bloco-10-testes-automatizados-com-jest/10.1/src/sum.js
+++ Fundamentos/bloco-10-testes-automatizados-com-jest/10.1/src/sum.js
@@ -1,4 +1,21 @@
-const sum = (a, b) => a + b;
+const sum = (a, b) => {
+  if (typeof (a) !== 'number' || typeof (b) !== 'number') {
+    throw new Error('parameters must be numbers');
+  };
+  return a + b;
+};
 
 
-module.exports = sum;
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+module.exports = {
+  sum,
+};
--- Fundamentos/bloco-10-testes-automatizados-com-jest/10.1/test/sum.test.js
+++ Fundamentos/bloco-10-testes-automatizados-com-jest/10.1/test/sum.test.js
@@ -1,5 +1,25 @@
-const sum = require('./src/test');
+const { sum } = require('../src/sum.js');
 
-test('soma dois valores', () => {
-  expect(sum(2, 3)).toBe(5);
-})
+describe('testa função de soma', () => {
+
+  test('4 mais 5 igual a 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  test('0 mais 0 igual a 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  // A função que lança uma exceção precisa ser chamada dentro de uma função de embrulho, caso contrário a afirmação toThrow falhará.
+
+  test('lança erro quando uma string é passada', () => {
+    expect(() => { sum(4, "5") }).toThrow()
+  });
+
+  test('lança mensagem de erro', () => {
+    expect(() => { sum(4, "5") }).toThrow('parameters must be numbers')
+
+  });
+
+});
+
