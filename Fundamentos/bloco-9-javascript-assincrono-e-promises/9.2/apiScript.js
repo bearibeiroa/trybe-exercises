9dd0a1fe10f13559053a38b31ffe84946364d5c3
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-08-17 18:55:10 +0100

    atualizando os arquivos

--- Fundamentos/bloco-9-javascript-assincrono-e-promises/9.2/apiScript.js
+++ Fundamentos/bloco-9-javascript-assincrono-e-promises/9.2/apiScript.js
@@ -7,11 +7,16 @@ const fetchJoke = () => {
     headers: { 'Accept': 'application/json' }
   };
 
-fetch(API_URL, {headers: { 'Accept': 'application/json' }})
-  .then(response => response.json())
-  .then(data => document.getElementById('jokeContainer').innerText = data.joke);
+  // fetch(API_URL, {headers: { 'Accept': 'application/json' }})
+  //   .then(response => response.json())
+  //   .then(data => document.getElementById('jokeContainer').innerText = data.joke);
+  // }
+
+  return fetch(API_URL, myObject)
+    .then (response => response.json())
+    .then(data => document.getElementById('jokeContainer').innerText = data.joke);
+
 }
 
 window.onload = () => fetchJoke();
-
 
