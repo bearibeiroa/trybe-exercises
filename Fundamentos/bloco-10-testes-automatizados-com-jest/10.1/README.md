2cc393c84bca04d6c19b8e95aab9906066bc28ff
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-08-17 21:31:48 +0100

    ajuste

--- Fundamentos/bloco-10-testes-automatizados-com-jest/10.1/README.md
+++
@@ -1,3 +0,0 @@
-# Instalando JEST
-
-1. Execute o comando `npm install --save-dev jest`
--- Fundamentos/bloco-10-testes-automatizados-com-jest/10.1/verificaSoma.test.js
+++
@@ -1,7 +0,0 @@
-const sum = require('./verificaSoma');
-
-describe('Requisito 1', () => {
-  it('A função recebe sum(4,5) e a soma é 9', () => {
-    expect(sum(4, 9).toEqual(9));
-  });
-})
