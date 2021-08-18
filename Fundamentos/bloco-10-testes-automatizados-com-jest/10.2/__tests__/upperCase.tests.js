b6a82ca50f77628034edbaee6ea3fd985d995874
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-08-18 22:20:38 +0100

    exercise 1

---
+++ Fundamentos/bloco-10-testes-automatizados-com-jest/10.2/__tests__/upperCase.tests.js
@@ -0,0 +1,13 @@
+const { uppercase } = require('../src/upperCase.js');
+const { it } = require('@jest/globals');
+
+it('uppercase "test" to equal "TEST"', (done) => {
+  uppercase('test', (str) => {
+    try {
+      expect(str).toBe('TEST');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
