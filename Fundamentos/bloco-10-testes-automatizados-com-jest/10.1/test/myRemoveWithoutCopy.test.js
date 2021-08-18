0d151caad357d01189ffa2e4b69de9add94be3eb
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-08-18 02:26:55 +0100

    questão 2 e 3

---
+++ Fundamentos/bloco-10-testes-automatizados-com-jest/10.1/test/myRemoveWithoutCopy.test.js
@@ -0,0 +1,24 @@
+const { myRemoveWithoutCopy } = require('../src/funcs.js');
+
+describe('A função myRemoveWithoutCopy', () => {
+
+  test('Existe', () => {
+    expect(typeof myRemoveWithoutCopy).toEqual('function');
+  });
+
+  test('a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna [1, 2, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  })
+
+  test('a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  })
+
+  // 3. tenho dúvida
+
+  test('a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna [1, 2, 3, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).not.toBe([1, 2, 3, 4]);
+  })
+
+
+});
