088a4a00446f806e66431eee8b0608ee2a11f840
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-08-18 23:35:21 +0100

    exercise 2

--- Fundamentos/bloco-10-testes-automatizados-com-jest/10.2/__tests__/exercise2_3.tests.js
+++ Fundamentos/bloco-10-testes-automatizados-com-jest/10.2/__tests__/exercise2_3.tests.js
@@ -1,12 +1,20 @@
 const { findUserById, getUserName } = require('../src/funcs.js');
-const { it } = require('@jest/globals');
+const { it, expect } = require('@jest/globals');
 
-// describe('verifica resultado da função getUserName', () => {
-//   it('encontra usuário', () => {
-//     findUserById('1').then((listUser) => {
-//       expect(listUser[0].name).toEqual('Mark');
-//       expect(listUser[1].name).toEqual('Paul')
-//     })
-//   })
+describe('verifica resultado da função getUserName', () => {
+  it('encontra usuário', () => {
+    findUserById(1).then((listUser) => {
+      expect(listUser.name).toEqual('Mark');
+    });
+  });
+});
 
-// })
+describe('testa o retorno de erro', () => {
+  findUserById('', () => {
+    try {
+      
+    } catch {
+
+    }
+  })
+})
