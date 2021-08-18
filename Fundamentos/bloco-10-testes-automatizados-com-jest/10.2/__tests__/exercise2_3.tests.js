173d40f3296338c65d1562f5e5585fe4c338ba15
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-08-18 23:06:10 +0100

    tentativa axercise 2 e 3

--- Fundamentos/bloco-10-testes-automatizados-com-jest/10.2/__tests__/exercise2_3.tests.js
+++ Fundamentos/bloco-10-testes-automatizados-com-jest/10.2/__tests__/exercise2_3.tests.js
@@ -1,2 +1,12 @@
 const { findUserById, getUserName } = require('../src/funcs.js');
-// const { it } = require('@jest/globals');
+const { it } = require('@jest/globals');
+
+// describe('verifica resultado da função getUserName', () => {
+//   it('encontra usuário', () => {
+//     findUserById('1').then((listUser) => {
+//       expect(listUser[0].name).toEqual('Mark');
+//       expect(listUser[1].name).toEqual('Paul')
+//     })
+//   })
+
+// })
