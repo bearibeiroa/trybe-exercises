9f08a8071a42f10b1e636ba484d2f842817cc9b7
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-08-24 10:59:21 +0100

    Resolução ex.5

--- Fundamentos/bloco-10-testes-automatizados-com-jest/10.2/beforeAfter.test.js
+++ Fundamentos/bloco-10-testes-automatizados-com-jest/10.2/beforeAfter.test.js
@@ -9,3 +9,12 @@ describe('Scoped / Nested block', () => {
 
   test('', () => console.log('2 - test'));
 });
+
+// 1 - beforeEach
+// 1 - test
+// 1 - afterEach
+// 1 - beforeEach
+// 2 - beforeEach
+// 2 - test
+// 2 - afterEach
+// 1 - afterEach
