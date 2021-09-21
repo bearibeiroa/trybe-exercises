a1eb04cbe71621bbfe77842472a6da23fece0b7e
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-21 13:11:52 +0100

    Refat e estilo

--- front-end/bloco-11-introducao-a-react/11.2/my-pokedex/src/Components/Pokedex.js
+++ front-end/bloco-11-introducao-a-react/11.2/my-pokedex/src/Components/Pokedex.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Pokemon from './Pokemon';
 
-{/* Nesse arquivo é onde eu listo os pokemons. Faço com eles apareçam na tela de forma dinâmica */}
+/* Nesse arquivo é onde eu listo os pokemons. Faço com que eles apareçam na tela de forma dinâmica através de um map() */
 class Pokedex extends React.Component {
   render() {
     const { pokemons } = this.props;
     return (
-      <div className="pokedex">
-        {pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon}/>)}
+      <div className="pokemon-map">
+        {pokemons.map(item => <Pokemon key={item.id} pokemon={item}/>)}
       </div>
     )
   }
